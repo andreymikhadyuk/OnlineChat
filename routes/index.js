1206bef5cdc03762a96a45fc0ddf01f8660691a0
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-08-09 00:30:40 +0300

    api/ prefix was added to routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const AuthController = require('../controllers/auth');
 const MessageController = require('../controllers/message');
 
-router.post('/login', AuthController.login);
-router.post('/signup', AuthController.signup);
+router.post('/api/login', AuthController.login);
+router.post('/api/signup', AuthController.signup);
 
-router.post('/message', AuthController.checkAuthorization, MessageController.saveMessage);
+router.post('/api/message', AuthController.checkAuthorization, MessageController.saveMessage);
 
 module.exports = router;
