891e2f67536ea79d09ee2af0617f2634ca5bd426
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-09-30 16:29:31 +0300

    Send user's data with omitted fields

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,12 @@
 const UserService = require('../services/UserService');
 const ResponseService = require('../services/response');
 
+const UserDto = require('../dto/UserDto');
+
 const getCurrentUserData = (req, res) => {
   if (!req.user) return ResponseService.sendUnauthorized(res);
   UserService.findById(req.user.id)
-    .then(user =>  ResponseService.sendOk(res, { user }))
+    .then(user =>  ResponseService.sendOk(res, { user: UserDto.convertToDto(user.toObject()) }))
     .catch(() => ResponseService.sendError(res));
 };
 
---
+++ dto/UserDto.js
@@ -0,0 +1,7 @@
+const omit = require('lodash/omit');
+
+const convertToDto = user => omit(user, ['password', 'createdAt']);
+
+module.exports = {
+  convertToDto,
+};
