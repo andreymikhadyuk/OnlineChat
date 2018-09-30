84140d2b099d104cccd362a4ebeb124450e11cb6
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2018-09-30 17:40:32 +0300

    Save message with createdAt field

--- controllers/message.js
+++ controllers/message.js
@@ -3,7 +3,8 @@ const ResponseService = require('../services/response');
 
 const saveMessage = (req, res) => {
   if (!req.user) return ResponseService.sendUnauthorized(res);
-  MessageService.saveMessage(req.user.id, req.body.text)
+  const { text, createdAt } = req.body;
+  MessageService.saveMessage(req.user.id, { text, createdAt })
     .then(() =>  ResponseService.sendOk(res))
     .catch(() => ResponseService.sendError(res));
 };
--- services/message.js
+++ services/message.js
@@ -1,13 +1,14 @@
 const Message = require('../models/message');
 
-const saveMessage = (userId, text) =>
+const saveMessage = (userId, { text, createdAt }) =>
   new Promise((resolve, reject) => {
     const message = new Message({
       userId,
       text,
+      createdAt,
     });
     message.save((err, newMessage) => {
-      if (err) return reject(err);
+      if (err) return console.error(err) && reject(err);
       resolve(newMessage);
     });
   });
